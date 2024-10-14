39d42dea9bd7dda857a2bb48e8e2f9d65ba2bf40
Javier Salazar <javier.slr137@gmail.com>
2024-10-14 03:13:48 -0300

    Más cambios

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: '/CarSharingJS/', // Asegúrate de usar el nombre exacto del repositorio
   plugins: [react()],
