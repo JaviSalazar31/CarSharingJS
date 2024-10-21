3891d76612de649a1d669448160091f1abc86647
Javier Salazar <javier.slr137@gmail.com>
2024-10-21 05:58:29 -0300

    Ajuste responsive

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Button from './Button';
-import CartWidget from './CartWidget';
 import '/src/HamburgerMenu.css';
 
 const HamburgerMenu = () => {
@@ -15,19 +14,21 @@ const HamburgerMenu = () => {
       <button className="hamburger-icon" onClick={toggleMenu}>
         &#9776; <span className="menu-text">Menú</span>
       </button>
-      {isOpen && (
-        <nav className="nav-links">
-          <Button text="Inicio" className="nav-link" onClick={() => window.location.href = "#home"} />
-          <Button text="Productos" className="nav-link" onClick={() => window.location.href = "#products"} />
-          <Button text="Contacto" className="nav-link" onClick={() => window.location.href = "#contact"} />
-          <CartWidget />
-        </nav>
-      )}
+      <nav className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <Button text="Inicio" className="nav-link" onClick={() => window.location.href = "#home"} />
+        <Button text="Productos" className="nav-link" onClick={() => window.location.href = "#products"} />
+        <Button text="Contacto" className="nav-link" onClick={() => window.location.href = "#contact"} />
+        <Button className="cart-link" text={<i className="fas fa-shopping-cart"></i>} onClick={() => window.location.href = "#cart"} />
+      </nav>
     </div>
   );
 };
 
 export default HamburgerMenu;
+
+
+
+
 
 
 
