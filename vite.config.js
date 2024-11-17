2cd794a32c8d80caa1197637e889193f4eadd534
Javier Salazar <javier.slr137@gmail.com>
2024-11-17 19:54:42 -0300

    ProyectoFinal+Salazar

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Tienda.JS/", // Asegúrate de usar esto para GitHub Pages
+  base: "/TIENDA.JS/", // Asegúrate de usar esto para GitHub Pages
   plugins: [react()],
 });
 
