51a58066081fcff3fef92e2fbb7c51f258efb2b2
Javier Salazar <javier.slr137@gmail.com>
2024-11-18 19:55:19 -0300

    ProyectoFinal+Salazar

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Tienda.JS/", // Esto asegura que las rutas tengan en cuenta el subdirectorio del proyecto
+  base: "/", // Esto asegura que las rutas tengan en cuenta el subdirectorio del proyecto
   plugins: [react()],
 });
 
